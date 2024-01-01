6c927f75f41c2257bc5e64a33ab37eb2bc05748a
Andrew Seatter <135391186+Andrew-seatter@users.noreply.github.com>
2024-01-01 11:07:34 -0800

    restructured code to fit into object tests

--- assets/shapes.js
+++ assets/shapes.js
@@ -1,4 +1,47 @@
-function logoShape(shape, shapeColor) {
-    this.shape = shape;
-    this.shapeColor = shapeColor;
+class Shape {
+    constructor(letters, textColor, shapeColor){
+        this.letters = letters;
+        this.textColor = textColor;
+        this.shapeColor = shapeColor;
+    }
+
+    render() {
+        throw new Error('Shape choice needs to be made');
+    }
+
 }
+
+class Circle extends Shape {
+    constructor(letters, textColor, shapeColor, shapeChoice){
+        super(letters, textColor, shapeColor);
+        this.shapeChoice = shapeChoice;
+    }
+
+    render() {
+        return `${shapeChoice}cx="150" cy="100" r="80" fill=${shapeColor}`;
+    }
+}
+
+class Triangle extends Shape {
+    constructor(letters, textColor, shapeColor, shapeChoice){
+        super(letters, textColor, shapeColor);
+        this.shapeChoice = shapeChoice;
+    }
+
+    render(){
+        return `polygon points="100 30, 200 200, 0 200" cx="150" cy="100" r="80" fill=${shapeColor}`;
+    }
+}
+
+class Square extends Shape {
+    constructor(letters, textColor, shapeColor, shapeChoice){
+        super(letters, textColor, shapeColor);
+        this.shapeChoice = shapeChoice;
+    }
+
+    render() {
+        return `rect x="50" y="20" width="150" height="150" fill=${shapeColor}`;
+    }
+}
+
+module.exports = {Shape, Circle, Triangle, Square};
